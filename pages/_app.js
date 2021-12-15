1bc4c84547bb775f00f4eec871604e111b00181a
Luke <llogan382@gmail.com>
2021-12-15 16:40:04 -0500

    adds another package

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import Amplify from "aws-amplify";
-import awsmobile from "./aws-exports";
+import awsmobile from "../aws-exports";
 
 Amplify.configure({
   ...awsmobile,
