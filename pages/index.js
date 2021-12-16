15f5244185b9f816229fa816062bc3c82a4e91d1
Luke <llogan382@gmail.com>
2021-12-15 21:36:39 -0500

    it works

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { DataStore } from "aws-amplify";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { Post } from "../src/models";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const postData = await DataStore.query(Post);
+      setPosts(postData);
+    }
+    fetchPosts();
+
+    const subscription = DataStore.observe(Post).subscribe(() => fetchPosts());
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <h1>Posts</h1>
+      {console.log(posts)}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>My Next.js Amplify app</h1>
-      </main>
+      {posts.map((post) => (
+        <Link key={post.id} href={`/posts/${post.id}`}>
+          <a>
+            <h2>{post.title}</h2>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 }
